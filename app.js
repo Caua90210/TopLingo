42079232f4cdbe70ed280b76d02fdd375c9d2d99
Cauã <124824095+Caua90210@users.noreply.github.com>
2024-03-26 11:26:14 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,92 +1,109 @@
-const traduzirTexto = document.querySelector("#traduzirTexto")
-const traducaoTexto = document.querySelector("#traducaoTexto")
-const btnTradutor = document.querySelector("#btnTradutor")
-const selects = document.querySelectorAll("select")
+const traduzirTexto = document.querySelector("#traduzirTexto");
+const traducaoTexto = document.querySelector("#traducaoTexto");
+const btnTradutor = document.querySelector("#btnTradutor");
+const selects = document.querySelectorAll("select");
 const btnToggleTheme = document.getElementById('btnToggleTheme');
-
-const chk = document.getElementById('chk')
+const speak = document.getElementById('btnSpeak');
+const voiceList = document.getElementById('voiceList');
+const synth = window.speechSynthesis;
+const chk = document.getElementById('chk');
+let voices = [];
 
 chk.addEventListener('change', () => {
-  document.body.classList.toggle('dark')
-})
+  document.body.classList.toggle('dark');
+});
 
-
-//Países disponíveis para tradução
+// Países disponíveis para tradução
 const paises = {
-    "pt-BR": "Português",
-    "en-US": "Inglês",
-    "fr-fr": "Francês",
-    "es-ES": "Espanhol",
-    "de-DE": "Alemão",
-    "ja-JP": "Japonês",
-    "nn-NO": "Norueguês"
-}
+  "pt-BR": "Português",
+  "en-US": "Inglês",
+  "fr-fr": "Francês",
+  "es-ES": "Espanhol",
+  "de-DE": "Alemão",
+  "ja-JP": "Japonês",
+  "nn-NO": "Norueguês"
+};
 
 selects.forEach((tag) => {
-
-    for (let pais in paises) {
-
-        let selected = ""
-
-        if(tag.classList.contains("selectFrom") && pais === "pt-BR") {
-            selected = "selected"
-
-        }else if(tag.classList.contains("selectTo") && pais === "en-US") {
-            selected = "selected"
-        }if(tag.classList.contains("selecTo") && pais == "fr-fr"){
-            selected = "selected"
-        }
-
-        const option = `<option value="${pais}"${selected}>${paises[pais]}</option>`
-
-        tag.insertAdjacentHTML("beforeend", option)
+  for (let pais in paises) {
+    let selected = "";
+    if (tag.classList.contains("selectFrom") && pais === "pt-BR") {
+      selected = "selected";
+    } else if (tag.classList.contains("selectTo") && pais === "en-US") {
+      selected = "selected";
+    } else if (tag.classList.contains("selecTo") && pais === "fr-fr") {
+      selected = "selected";
     }
-})
-
-
+    const option = `<option value="${pais}"${selected}>${paises[pais]}</option>`;
+    tag.insertAdjacentHTML("beforeend", option);
+  }
+});
 
 btnTradutor.addEventListener("click", () => {
-    
-    if(traduzirTexto.value) {
-        loadTranslation()
-    }else {
-        traducaoTexto.value = ""
-    }
-    if (traduzirTexto.value.trim().toLowerCase() === "alice") {
-        document.body.classList.toggle('dark')
-    }
-    //  else {
-    //     document.body.classList.remove("dark");
-    // }
-   
-
-
-    
-})
+  if (traduzirTexto.value) {
+    loadTranslation();
+  } else {
+    traducaoTexto.value = "";
+  }
+  if (traduzirTexto.value.trim().toLowerCase() === "alice") {
+    document.body.classList.toggle('dark');
+  }
+});
 
 function loadTranslation() {
-
-    fetch(
-        `https://api.mymemory.translated.net/get?q=${traduzirTexto.value}&langpair=${selects[0].value}|${selects[1].value}`
+  fetch(
+      `https://api.mymemory.translated.net/get?q=${traduzirTexto.value}&langpair=${selects[0].value}|${selects[1].value}`
     )
-
     .then((res) => res.json())
     .then((data) => {
-        traducaoTexto.value = data.responseData.translatedText
-    })
+      traducaoTexto.value = data.responseData.translatedText;
+    });
 }
 
 btnToggleVoice.addEventListener('click', () => {
-    const recognition = new (window.SpeechRecognition || window.webkitSpeechRecognition || window.mozSpeechRecognition || window.msSpeechRecognition)();
-    recognition.lang = 'pt-BR';
-    recognition.lang = 'fr-FR';
-    recognition.lang = 'en-US';
-    
+  const recognition = new(window.SpeechRecognition || window.webkitSpeechRecognition || window.mozSpeechRecognition || window.msSpeechRecognition)();
+  recognition.lang = 'pt-BR';
+  recognition.lang = 'fr-FR';
+  recognition.lang = 'en-US';
 
-    recognition.onresult = (event) => {
-        const speechResult = event.results[0][0].transcript;
-        traduzirTexto.value = speechResult;
-    };
+  recognition.onresult = (event) => {
+    const speechResult = event.results[0][0].transcript;
+    traduzirTexto.value = speechResult;
+  };
 
-    recognition.start();
+  recognition.start();
 });
+
+speak.addEventListener('click', () => {
+
+  if (voices.length > 0) {
+    const toSpeak = new SpeechSynthesisUtterance(traducaoTexto.value);
+    const selectedVoiceName = voiceList.options[voiceList.selectedIndex].getAttribute('data-name');
+    voices.forEach((voice) => {
+      if (voice.name === selectedVoiceName) {
+        toSpeak.voice = voice;
+      }
+    });
+    synth.speak(toSpeak);
+  } else {
+    console.error('Não há vozes disponíveis.');
+  }
+});
+
+function NewVoices() {
+  voices = synth.getVoices();
+  const selectedIndex = voiceList.selectedIndex < 0 ? 0 : voiceList.selectedIndex;
+  voiceList.innerHTML = '';
+  voices.forEach((voice) => {
+    const listItem = document.createElement('option');
+    listItem.textContent = voice.name;
+    listItem.setAttribute('data-lang', voice.lang);
+    listItem.setAttribute('data-name', voice.name);
+    voiceList.appendChild(listItem);
+  });
+  voiceList.selectedIndex = selectedIndex;
+}
+
+if (speechSynthesis !== undefined) {
+  speechSynthesis.onvoiceschanged = NewVoices;
+}
